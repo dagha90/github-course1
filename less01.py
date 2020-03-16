b8d1fed2878874856b8e26a1c937d405c2305436
dagha90 <tfh90@hotmail.com>
2020-03-16 15:16:50 +0300

    add comand

---
+++ less01.py
@@ -0,0 +1,10 @@
+print("Hi MarMar")
+name = "Talal"
+age = "30"
+born = "Hail"
+lived = "Riyadh"
+
+print("I am " +name)
+print("I am " +age+ " years old")
+print("I am from " +born)
+print("I am living in " +lived)
